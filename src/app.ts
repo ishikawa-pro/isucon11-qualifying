a58093ef2f204386d5be086a8ecdd9f6e5c263c0
ishikawa-pro <ishikawa.pro@gmail.com>
2021-08-21 11:42:43 +0900

    refactor: post condition

--- src/app.ts
+++ src/app.ts
@@ -1133,44 +1133,37 @@ app.post(
     try {
       const jiaIsuUUID = req.params.jia_isu_uuid;
 
-      const request = req.body;
+      const request: PostIsuConditionRequest[] = req.body;
       if (!isValidPostIsuConditionRequest(request) || request.length === 0) {
         return res.status(400).type("text").send("bad request body");
       }
-
-      await db.beginTransaction();
 
       const [[{ cnt }]] = await db.query<(RowDataPacket & { cnt: number })[]>(
         "SELECT COUNT(*) AS `cnt` FROM `isu` WHERE `jia_isu_uuid` = ?",
         [jiaIsuUUID]
       );
       if (cnt === 0) {
-        await db.rollback();
         return res.status(404).type("text").send("not found: isu");
       }
 
-      for (const cond of request) {
+      if (request.some(cond => !isValidConditionFormat(cond.condition))) {
+        return res.status(400).type("text").send("bad request body");
+      }
+      const payload = request.map(cond => {
         const timestamp = new Date(cond.timestamp * 1000);
-
-        if (!isValidConditionFormat(cond.condition)) {
-          await db.rollback();
-          return res.status(400).type("text").send("bad request body");
-        }
-
-        await db.query(
+        return `(${jiaIsuUUID}, ${timestamp}, ${cond.is_sitting}, ${cond.condition}, ${cond.message})`
+      }).join();
+      await db.query(
           "INSERT INTO `isu_condition`" +
             "	(`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`)" +
-            "	VALUES (?, ?, ?, ?, ?)",
-          [jiaIsuUUID, timestamp, cond.is_sitting, cond.condition, cond.message]
-        );
-      }
-
-      await db.commit();
+            "	VALUES" +
+          payload
+      )
 
       return res.status(202).send();
     } catch (err) {
       console.error(`db error: ${err}`);
-      await db.rollback();
+      // await db.rollback();
       return res.status(500).send();
     } finally {
       db.release();
